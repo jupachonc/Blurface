b08ab542425df7141383e59529138be3c25dec87
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-01 23:45:02 -0500

    fsdsdfdsdf

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -72,7 +72,7 @@ int step, int width, int height, int initX, int initY, int numThreads, int fullM
                 //printf("%d, %d", col, row);
                 
                 rMatrix[(3 * step * row) + (3 * col) + 0] = 27756;
-                print("%hu\n", rMatrix[(3 * step * row) + (3 * col) + 0] );
+                printf("%hu\n", rMatrix[(3 * step * row) + (3 * col) + 0] );
                 rMatrix[(3 * step * row) + (3 * col) + 1] = 27756;
                 rMatrix[(3 * step * row) + (3 * col) + 2] = 27756;
             }
