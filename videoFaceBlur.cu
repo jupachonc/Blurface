bcc10c8c25de6297f815e7a8c29b184f3b781480
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-02 01:15:09 -0500

    Pleaseeeeeee

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -29,13 +29,13 @@ int step, int width, int height, int initX, int initY, int numThreads, int fullM
     int partition = (width * height) / numThreads;
     
     //printf("Device width %d \n", width);
-    int start_x = (threadId * partition) % fullMatrixSize;
+    int start_x = (int) (threadId * partition) / fullMatrixSize;
 
-    int start_y = (int) (threadId * partition) / fullMatrixSize;
+    int start_y = (int) (threadId * partition) / matrixSize1D;
 
-    int end_x = ((threadId + 1) * partition) % fullMatrixSize - 1;
+    int end_x = (int) ((threadId + 1) * partition) / fullMatrixSize - 1;
 
-    int end_y = (int) (threadId + 1 * partition) / fullMatrixSize - 1;
+    int end_y = (int) (threadId + 1 * partition) / matrixSize1D -1;
 
     
 
