22db66911b0513239fedb00ff0c0c08950b0247d
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-01 23:26:35 -0500

    4tgdfg

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -69,9 +69,9 @@ int step, int width, int height, int initX, int initY, int numThreads, int fullM
 
                 printf("%d, %d", col, row);
                 
-                *(rMatrix + ((3 * step * row) + (3 * col) + 0)) = (short) 255;
-                *(rMatrix + ((3 * step * row) + (3 * col) + 1)) = (short) 255;
-                *(rMatrix + ((3 * step * row) + (3 * col) + 2)) = (short) 255;
+                *(rMatrix + ((3 * step * row) + (3 * col) + 0)) = (short) 2;
+                *(rMatrix + ((3 * step * row) + (3 * col) + 1)) = (short) 2;
+                *(rMatrix + ((3 * step * row) + (3 * col) + 2)) = (short) 2;
             }
 
         
