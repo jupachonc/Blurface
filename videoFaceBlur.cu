223bba30fff3256f9f2645f8f72b141e0cd7ce26
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-02 00:00:56 -0500

    211

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -261,7 +261,7 @@ int main(int argc, char *argv[]){
     //  CV_CAP_PROP_POS_FRAMES : Index of the next frame.
 
     int i = 0;
-    while (i < frame_count)
+    while (i < 100)
     {
         cap.set(CAP_PROP_POS_FRAMES, i);
         Mat frame;
