c6c307261a593144a1d1e2f18cbf52ba84a1dcd3
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-01 21:18:57 -0500

    asddasdas

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -131,7 +131,7 @@ void detectAndBlur(Mat &img, CascadeClassifier &cascade){
             h_G = (short *)G.data;
             h_R = (short *)R.data;
 
-            cout << size << endl;
+            cout << sizeof(h_B) << endl;
 
             err = cudaMalloc((void **) &d_B, size);
 
