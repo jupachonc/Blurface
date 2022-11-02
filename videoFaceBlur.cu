f2cd1932451e7b9ae33e0b43467ae6b5ab1efa9a
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-01 23:09:33 -0500

    P13

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -65,9 +65,9 @@ int step, int width, int height, int initX, int initY, int numThreads, int fullM
                 int col = x + (i % matrixSize1D);
                 int row = y + (int)(i / matrixSize1D);
                 
-                rMatrix[(3 * step * row) + (3 * col) + 0] = 255;
-                rMatrix[(3 * step * row) + (3 * col) + 1] = 255;
-                rMatrix[(3 * step * row) + (3 * col) + 2] = 255;
+                *rMatrix[(3 * step * row) + (3 * col) + 0] = 255;
+                *rMatrix[(3 * step * row) + (3 * col) + 1] = 255;
+                *rMatrix[(3 * step * row) + (3 * col) + 2] = 255;
             }
 
         
