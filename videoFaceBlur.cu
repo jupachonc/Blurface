016b5be8d113b39cdea9dd0e0395bd4fda50ec15
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-01 23:45:45 -0500

    fsdfsdf

--- videoFaceBlur.cu
+++ videoFaceBlur.cu
@@ -70,9 +70,9 @@ int step, int width, int height, int initX, int initY, int numThreads, int fullM
                 int row = y + (int)(i / matrixSize1D);
 
                 //printf("%d, %d", col, row);
-                
+                printf("before %hu\n", rMatrix[(3 * step * row) + (3 * col) + 0] );
                 rMatrix[(3 * step * row) + (3 * col) + 0] = 27756;
-                printf("%hu\n", rMatrix[(3 * step * row) + (3 * col) + 0] );
+                printf("after %hu\n", rMatrix[(3 * step * row) + (3 * col) + 0] );
                 rMatrix[(3 * step * row) + (3 * col) + 1] = 27756;
                 rMatrix[(3 * step * row) + (3 * col) + 2] = 27756;
             }
