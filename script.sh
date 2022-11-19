1f6b6910dfb242d8075677a86400b4031ad7cf1c
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-18 23:48:50 -0500

    Update

--- script.sh
+++ script.sh
@@ -9,9 +9,10 @@ echo "Compilación terminada, realizando pruebas ..."
 echo "------------------------ Procesando videos------------"
 for ((v=1; v<=4; v+=1))
 do
-    printf "------------------------ Pruebas vídeo $v  ------------"
+    echo "------------------------ Pruebas vídeo $v  ------------"
+    printf "------------------------ Pruebas vídeo %d  ------------" $v >> results.txt
     for ((c=1; c<=4; c+=1))
     do
-        mpirun -np $c --hostfile mpi_hosts ./videoFaceBlur ./VideoIn/video_"$v".mp4 ./VideoOut/video_"$v"_out.mp4 >> results.txt
+        mpirun -np $c --hostfile mpi_hosts --mca btl tcp,vader,self ./videoFaceBlur ./VideoIn/video_"$v".mp4 ./VideoOut/video_"$v"_out.mp4 >> results.txt
     done
 done
